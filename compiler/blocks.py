de9b4f2be073a13f27e3deea713324d0a9acdf3d
wim-c <73782798+wim-c@users.noreply.github.com>
2021-08-08 17:48:41 +0200

    Add flow analysis on marks.
    Add flow analysis on marks.

--- compiler/blocks.py
+++ compiler/blocks.py
@@ -1,15 +1,47 @@
 
+import tokens
+
 class CompiledBlock:
     def __init__(self, nodes):
         super().__init__()
         self.nodes = nodes
+        self.reachable = False
+
+        for index, node in enumerate(self.nodes):
+            if isinstance(node, tokens.Mark):
+                node.block = self
+                node.index = index
+
+    def set_reachable_from_index(self, start, marks_to_visit):
+        for index in range(start, len(self.nodes)):
+            if not self.nodes[index].set_reachable(marks_to_visit):
+                break
 
 
 class CodeBlock(CompiledBlock):
+    def __init__(self, nodes):
+        super().__init__(nodes)
+        self.first_reachable_index = None
+
     def assign_address(self, address):
         for node in self.nodes:
             address = node.assign_address(address)
         return address
+
+    def set_reachable_from_index(self, start, marks_to_visit):
+        if self.reachable:
+            self.first_reachable_index = min(self.first_reachable_index, start)
+        else:
+            self.reachable = True
+            self.first_reachable_index = start
+        super().set_reachable_from_index(start, marks_to_visit)
+
+    def visit(self):
+        marks_to_visit = set()
+        self.set_reachable_from_index(0, marks_to_visit)
+
+        while len(marks_to_visit) > 0:
+            marks_to_visit.pop().set_used(marks_to_visit)
 
 
 class DataBlock(CompiledBlock):
@@ -17,3 +49,8 @@ class DataBlock(CompiledBlock):
         for node in self.nodes:
             address = node.assign_word_address(address)
         return address
+
+    def set_reachable_from_index(self, start, marks_to_visit):
+        if not self.reachable:
+            self.reachable = True
+            super().set_reachable_from_index(0, marks_to_visit)
