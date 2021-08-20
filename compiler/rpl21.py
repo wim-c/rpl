62e96a3c8247714b77d1ba0401dfb227565c329e
wim-c <73782798+wim-c@users.noreply.github.com>
2021-08-20 21:16:37 +0200

    Fix help text.
    Fix command line help text for -r option. Drop $ notation for hex number.

--- compiler/rpl21.py
+++ compiler/rpl21.py
@@ -6,15 +6,13 @@ import formatter
 def dex(txt):
     if txt[0:2] == '0x':
         return int(txt[2:], 16)
-    elif txt[0] == '$':
-        return int(txt[1:], 16)
     else:
         return int(txt)
 
 
 parser = argparse.ArgumentParser(description='RPL21 compiler (c) 2021 Wim Couwenberg')
 parser.add_argument('-l', '--location', dest='org', metavar='ADDRESS', default=0x401, type=dex, help='The address where the compiled program will be loaded.  Default is 0x401.')
-parser.add_argument('-r', '--runtime', dest='rt', metavar='ADDRESS', default=0x9000, type=dex, help='The address where the RPL12 runtime code is located.  Default is 0x9000.')
+parser.add_argument('-r', '--runtime', dest='rt', metavar='ADDRESS', default=0x9000, type=dex, help='The address where the RPL21 runtime code is located.  Default is 0x9000.')
 parser.add_argument('-f', '--format', dest='fmt', default='print', choices=['print', 'basic', 'bin'], help='The format of the generated output.')
 parser.add_argument('-o', '--output', dest='output', metavar='FILE', default=None, help='Output will be written to this file path.  Default is stdout.')
 parser.add_argument('-p', '--prg', dest='prg', default=False, action='store_true', help='Start the output with a two byte location header.  Has no effect for the output type \'print\'.')
